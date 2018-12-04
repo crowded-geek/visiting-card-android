0826a2ffc1735faefbb535ab8da83e9931a1eb16
Aayushman Choudhary <Aayushmanchaudhory@gmail.com>
2018-12-05 01:20:08 +0530

    Merges (#1)
    * Create LICENSE.md
    
    Added MIT License
    
    * Fixed Typos
    
    Completed for Google Code-in Task.
    
    * Added profile pic change functionality
    
    * Added dependencies
    
    * Added Splashscreen and initial app tutorial (#123)
    
    * visiting card design (#128)
    
    Removed Action bar
    
    * New Logo Added (#113)

---
+++ app/src/main/java/com/community/jboss/visitingcard/visitingcard/VisitingCardActivity.java
@@ -0,0 +1,121 @@
+package com.community.jboss.visitingcard.visitingcard;
+
+import android.content.Intent;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import com.community.jboss.visitingcard.R;
+import com.community.jboss.visitingcard.SettingsActivity;
+import com.community.jboss.visitingcard.maps.MapsActivity;
+import java.io.IOException;
+import siclo.com.ezphotopicker.api.EZPhotoPick;
+import siclo.com.ezphotopicker.api.EZPhotoPickStorage;
+import siclo.com.ezphotopicker.api.models.EZPhotoPickConfig;
+import siclo.com.ezphotopicker.api.models.PhotoSource;
+import android.widget.ImageButton;
+
+public class VisitingCardActivity extends AppCompatActivity {
+
+    private ImageButton profile_img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.visiting_card);
+
+	profile_img=findViewById(R.id.profile_img);
+
+        // TODO: Add a ImageView and a number of EditText to get his/her Visiting Card details (Currently authenticated User)
+
+        // TODO: Add profileImage, Name, Email, PhoneNumber, Github, LinkedIn & Twitter Fields.
+
+        // TODO: Clicking the ImageView should invoke an implicit intent to take an image using camera / pick an image from the Gallery.
+
+        // TODO: Align FAB to Bottom Right and replace it's icon with a SAVE icon
+        // TODO: On Click on FAB should make a network call to store the entered information in the cloud using POST method(Do this in NetworkUtils class)
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Proceed to Maps Activity", Snackbar.LENGTH_LONG)
+                        .setAction("Yes", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent toVisitingCard = new Intent(VisitingCardActivity.this, MapsActivity.class);
+                                startActivity(toVisitingCard);
+                            }
+                        }).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == EZPhotoPick.PHOTO_PICK_GALLERY_REQUEST_CODE || requestCode == EZPhotoPick.PHOTO_PICK_CAMERA_REQUEST_CODE) {
+            Bitmap pickedPhoto = null;
+            try {
+                pickedPhoto = new EZPhotoPickStorage(this).loadLatestStoredPhotoBitmap();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            profile_img.setImageBitmap(pickedPhoto);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(VisitingCardActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    public void select_img(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Select image source")
+                .setCancelable(false)
+                .setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        EZPhotoPickConfig config = new EZPhotoPickConfig();
+                        config.photoSource = PhotoSource.GALLERY; // or PhotoSource.CAMERA
+                        config.isAllowMultipleSelect = false; // only for GALLERY pick and API >18
+                        EZPhotoPick.startPhotoPickActivity(VisitingCardActivity.this, config);
+                    }
+                })
+                .setNegativeButton("Camera", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        EZPhotoPickConfig config = new EZPhotoPickConfig();
+                        config.photoSource = PhotoSource.CAMERA; // or PhotoSource.CAMERA
+                        config.isAllowMultipleSelect = false; // only for GALLERY pick and API >18
+                        EZPhotoPick.startPhotoPickActivity(VisitingCardActivity.this, config);
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
